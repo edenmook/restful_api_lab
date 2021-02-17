f400612510d641b6d29f7c8c6be1d3204a456632
mook <615021000315@mail.rmutk.ac.th>
2021-02-17 12:18:36 +0700

    up 1

---
+++ react/test/src/Appheader.js
@@ -0,0 +1,15 @@
+
+
+function Appheader() {
+
+
+    return (
+    
+          <header className="App-header">
+              <img className ="header-logo" src ="/pic/m3.jpg"/>
+    
+    
+          </header>
+        );
+        }
+        export default Appheader;
---
+++ react/test/src/items.js
@@ -0,0 +1,28 @@
+const items =[
+    {
+      title :"boyfriend",
+      thumbnaiUrl :"/pic/marklee.jpg"
+      
+    
+    },
+    {
+      title :"my boy",
+      thumbnaiUrl :"/pic/m1.png"
+      
+    
+    },
+    {
+      title :"Baby",
+      thumbnaiUrl :"/pic/m2.jpg"
+      
+    
+    },
+    {
+      title :"maow",
+      thumbnaiUrl :"/pic/m3.jpg"
+      
+    
+    }
+  ];
+  export default items;
+
